f00db5d9ba4cf5f3cbccac31fb4bb6cc49d11967
arthurssrichard <141867186+arthurssrichard@users.noreply.github.com>
2025-01-03 21:35:52 -0300

    feat(categories): add error messages and validation

--- resources/views/livewire/create-category.blade.php
+++ resources/views/livewire/create-category.blade.php
@@ -7,15 +7,24 @@
                 <div>
                     <label for="name" class="block text-sm font-medium mb-2 dark:text-white">Nome</label>
                     <input type="text" class="text-input-default" wire:model="name" wire:keyup="generateSlug" id="name" placeholder="Nome da categoria">
+                    @error('name')
+                    <span class="text-red-600">{!!$message!!}</span>
+                    @enderror
                 </div>
                 <div>
                     <label for="slug" class="block text-sm font-medium mb-2 dark:text-white">Slug</label>
                     <input type="text" id="slug" class="text-input-default" wire:model="slug" placeholder="slug-da-categoria">
+                    @error('slug')
+                    <span class="text-red-600">{!!$message!!}</span>
+                    @enderror
                 </div>
                 <div class="flex flex-row space-x-10 items-end">
                     <div>
                         <label for="hs-color-input" class="block text-sm font-medium mb-2 dark:text-white">Cor</label>
                         <input type="color" class="p-1 h-10 w-14 block bg-white border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700" id="hs-color-input" value="#2563eb" title="Escolha a cor da categoria" wire:model="color">
+                        @error('color')
+                        <span class="text-red-600">{!!$message!!}</span>
+                        @enderror
                     </div>
                     <button class="btn-default bg-blue-600 text-white hover:bg-blue-700 focus:bg-blue-700">{{$categoryId ? "Editar" : "Adicionar"}}</button>                
                 </div>
