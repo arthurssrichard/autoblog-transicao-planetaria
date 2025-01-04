80e3781f1e80f90fb163601c4cdd9da06b7612fc
arthurssrichard <141867186+arthurssrichard@users.noreply.github.com>
2025-01-03 22:40:03 -0300

    style: adapt blogadmin/categories page to dark and responsive mode

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.main')
 @section('title','Categories')
 @section('content')
-<h2>Categorias</h2>
-<main class="w-full flex flex-row justify-center">
-    <div class="w-8/12"><livewire:admin-category-list/></div>
+<main class="w-full flex flex-row justify-center p-5">
+    <div class="w-8/12">
+        <h2 class="mb-5 text-3xl font-bold dark:text-neutral-300">Categorias</h2>
+        <livewire:admin-category-list/>
+    </div>
 </main>
 @livewireStyles
 @livewireScripts
--- resources/views/livewire/admin-category-list.blade.php
+++ resources/views/livewire/admin-category-list.blade.php
@@ -1,39 +1,39 @@
-<div class="flex flex-col rounded-lg border">
+<div class="flex flex-col rounded-lg border dark:border-neutral-500">
     @if(session()->has('error'))
         <x-error-alert title="Erro" message="{{ session('error') }}"/>
     @endif
     <div class="p-4">
         <div class="relative lg:w-6/12 min-w-6/12">
-            <input type="text" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 ps-8 pe-0" wire:model.live.debounce.300ms="search" placeholder="Pesquisar">
+            <input type="text" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-950 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 ps-8 pe-0" wire:model.live.debounce.300ms="search" placeholder="Pesquisar">
 
             <div class="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-2 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-                <ion-icon name="search-outline" wire:ignore></ion-icon>
+                <ion-icon class="dark:text-neutral-400" name="search-outline" wire:ignore></ion-icon>
             </div>
         </div>
     </div>
-    <div>
+    <div class="dark:bg-neutral-800">
         <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
-            <thead class="bg-gray-100 dark:bg-neutral-700">
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Nome</th>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Slug</th>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Cor</th>
-                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Ação</th>
+            <thead class="bg-gray-100 dark:bg-neutral-900">
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 table-cell sm:table-cell">Nome</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 table-cell sm:table-cell">Slug</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden sm:table-cell">Cor</th>
+                <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500 hidden sm:table-cell">Ação</th>
             </thead>
             <tbody> @foreach($categories as $category)
-                <tr class="border-b">
-                    <td class="px-6 py-4 whitespace-normal text-sm font-medium text-gray-800 dark:text-neutral-200 text-start text-ellipsis">
+                <tr class="border-b dark:border-neutral-500">
+                    <td class="px-6 py-4 whitespace-normal text-sm font-medium text-gray-800 dark:text-neutral-300 text-start text-ellipsis table-cell sm:table-cell">
                         <a href="/blogadmin/categories/{{$category->id}}/edit" wire:navigate class="hover:underline">{{$category->name}}</a>
                     </td>
 
-                    <td class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-800 dark:text-neutral-200 text-start">
+                    <td class="px-6 py-4 whitespace-nowrap text-xs font-medium text-gray-800 dark:text-neutral-300 text-start table-cell sm:table-cell">
                         {{$category->slug}}
                     </td>
 
-                    <td class="px-6 py-4 whitespace-normal text-sm font-medium text-gray-800 dark:text-neutral-200 ">
+                    <td class="px-6 py-4 whitespace-normal text-sm font-medium text-gray-800 dark:text-neutral-300 hidden sm:table-cell">
                         <div class="rounded-lg w-6/12 sm:w-10/12" style="height: 40px; background-color: {{$category->color;}}"></div>
                     </td>
 
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200 text-start">
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-300 text-start hidden sm:table-cell">
                         <button wire:click="delete({{$category->id}})">
                             <ion-icon name="trash-outline" class="text-red-500 text-xl" wire:ignore></ion-icon>
                         </button>
