b9f4050f52cf9f0af154c5eff5dd009603cd6c86
arthurssrichard <arthurssrichard@gmail.com>
2025-01-26 11:29:43 -0300

    fix(posts): tinymce not loading correctly on /create

--- resources/views/components/input/tinymce.blade.php
+++ resources/views/components/input/tinymce.blade.php
@@ -1,37 +1,39 @@
 <div
     x-data="{ value: @entangle($attributes->wire('model')) }"
     x-init="
-        const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        const updateEditorTheme = () => isDarkMode ? 'oxide-dark' : 'oxide';
+        setTimeout(() => {
+            const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            const updateEditorTheme = () => isDarkMode ? 'oxide-dark' : 'oxide';
 
-        tinymce.init({
-            target: $refs.tinymce,
-            skin: updateEditorTheme(),
-            content_css: isDarkMode ? 'dark' : '',
-            themes: 'modern',
-            height: 200,
-            menubar: false,
-            branding: false,
-            setup: function(editor) {
-                
-                editor.on('blur', function(e) {
-                    value = editor.getContent();
-                });
+            tinymce.init({
+                target: $refs.tinymce,
+                skin: updateEditorTheme(),
+                content_css: isDarkMode ? 'dark' : '',
+                themes: 'modern',
+                height: 200,
+                menubar: false,
+                branding: false,
+                setup: function(editor) {
+                    
+                    editor.on('blur', function(e) {
+                        value = editor.getContent();
+                    });
 
-                editor.on('init', function(e) {
-                if(value !== null   ){
-                    editor.setContent(value);
-                }
-                });
-
-                
-                $watch('value', function(newValue) {
-                    if (newValue !== editor.getContent()) {
-                        editor.setContent(newValue || '');
+                    editor.on('init', function(e) {
+                    if(value !== null   ){
+                        editor.setContent(value);
                     }
-                });
-            }
-        });
+                    });
+
+                    
+                    $watch('value', function(newValue) {
+                        if (newValue !== editor.getContent()) {
+                            editor.setContent(newValue || '');
+                        }
+                    });
+                }
+            });
+        }, 500);
     "
     wire:ignore>
     <div>
